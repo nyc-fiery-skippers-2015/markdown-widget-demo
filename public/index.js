44bd931d00abd3c30f237d8a4f44d7027d224cad
Steven G. Harms <steven@devbootcamp.com>
2015-06-15 12:13:07 -0400

    Working OO style implementation

--- public/index.js
+++ public/index.js
@@ -1,5 +1,42 @@
+MarkdownPreviewer = {};
+
+MarkdownPreviewer.SourceView = function(selector) {
+  this.$elem = $(selector);
+}
+
+MarkdownPreviewer.SourceView.prototype = {
+  getText: function() {
+    return this.$elem.val();
+  }, 
+
+  setText: function(newText) {
+    this.$elem.val(newText);
+    return newText;
+  }
+}
+
+MarkdownPreviewer.PreviewView = function(selector) {
+  this.$elem = $(selector);
+}
+
+MarkdownPreviewer.PreviewView.prototype.setText = function(data) {
+  this.$elem.html(data)
+}
+
+MarkdownPreviewer.Controller = function(sourceView, destView) {
+  this.sourceView = sourceView;
+  this.destView = destView;
+
+  this.sourceView.$elem.on("keyup", function() {
+    this.destView.setText(this.convertMarkdown(this.sourceView.getText()));
+  }.bind(this));
+}
+
+MarkdownPreviewer.Controller.prototype.convertMarkdown = function(mdText)  {
+  return marked(mdText);
+}
+
 $(document).ready(function() {
-  $("#markdown-source").on('keyup', function(event) {
-    $("#display").html(marked($("#markdown-source").val()))
-  })
+  new MarkdownPreviewer.Controller(new MarkdownPreviewer.SourceView("#markdown-source"),
+                                   new MarkdownPreviewer.PreviewView("#preview"));
 });
